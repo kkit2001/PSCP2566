97474eb8a8f82c74173fde915b9c5437b79291d9
kkit2001 <73679898+kkit2001@users.noreply.github.com>
2023-09-30 13:31:35 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,88 @@
+#=====For IMPORT EVERYTHING=====
+from tkinter import *
+import sqlite3
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+
+
+#=====MAIN WINDOW=====
+root = Tk()
+root.title("Contact List Project For PSCP2023")
+width = 800
+height = 400
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+root.config(bg="#feebbd")
+
+#=====VARIABLES=====
+FIRSTNAME = StringVar()
+LASTNAME = StringVar()
+GENDER = StringVar()
+AGE = StringVar()
+ADDRESS = StringVar()
+CONTACT = StringVar()
+
+#=====DataBaseCreate=====
+def Database():
+    conn = sqlite3.connect("PSCPproject.db")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `member` (mem_id INTEGER NOT NULL  PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, gender TEXT, age TEXT, address TEXT, contact TEXT)")
+    cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
+    fetch = cursor.fetchall()
+    for data in fetch:
+        tree.insert('', 'end', values=(data))
+    cursor.close()
+    conn.close()
+
+#=====FrameOnTop=====
+Top = Frame(root, width=500, bd=1, relief=SOLID)
+Top.pack(side=TOP)
+Mid = Frame(root, width=500,  bg="#feebbd")
+Mid.pack(side=TOP)
+MidLeft = Frame(Mid, width=100)
+MidLeft.pack(side=LEFT, pady=10)
+MidLeftPadding = Frame(Mid, width=370, bg="#feebbd")
+MidLeftPadding.pack(side=LEFT)
+MidRight = Frame(Mid, width=100)
+MidRight.pack(side=RIGHT, pady=10)
+TableMargin = Frame(root, width=500)
+TableMargin.pack(side=TOP)
+
+#=====Label On Top=====
+lbl_title = Label(Top, text="PSCP Contact Management System",bg="#ffc77b", font=('arial', 16), width=500)
+lbl_title.pack(fill=X)
+
+#=====Show Formation=====
+scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
+scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
+tree = ttk.Treeview(TableMargin, columns=("MemberID", "Firstname", "Lastname", "Gender", "Age", "Address", "Contact"), height=400, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+scrollbary.config(command=tree.yview)
+scrollbary.pack(side=RIGHT, fill=Y)
+scrollbarx.config(command=tree.xview)
+scrollbarx.pack(side=BOTTOM, fill=X)
+tree.heading('MemberID', text="MemberID", anchor=W)
+tree.heading('Firstname', text="Firstname", anchor=W)
+tree.heading('Lastname', text="Lastname", anchor=W)
+tree.heading('Gender', text="Gender", anchor=W)
+tree.heading('Age', text="Age", anchor=W)
+tree.heading('Address', text="Address", anchor=W)
+tree.heading('Contact', text="Contact", anchor=W)
+tree.column('#0', stretch=NO, minwidth=0, width=0)
+tree.column('#1', stretch=NO, minwidth=0, width=0)
+tree.column('#2', stretch=NO, minwidth=0, width=80)
+tree.column('#3', stretch=NO, minwidth=0, width=120)
+tree.column('#4', stretch=NO, minwidth=0, width=90)
+tree.column('#5', stretch=NO, minwidth=0, width=80)
+tree.column('#6', stretch=NO, minwidth=0, width=120)
+tree.column('#7', stretch=NO, minwidth=0, width=120)
+tree.pack()
+
+
+if __name__ == '__main__':
+    Database()
+    root.mainloop()
+    
