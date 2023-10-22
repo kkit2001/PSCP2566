c41a1ba55f28063a8d6527229e3c1638be1fd86c
kkit2001 <63070009@kmitl.ac.th>
2023-10-22 20:16:17 +0700

    update

--- index.py
+++ index.py
@@ -38,6 +38,214 @@ def Database():
     cursor.close()
     conn.close()
 
+#=====AddNewDataBase=====
+def SubmitData():
+    if  FIRSTNAME.get() == "" or LASTNAME.get() == "" or GENDER.get() == "" or AGE.get() == "" or ADDRESS.get() == "" or CONTACT.get() == "":
+        result = tkMessageBox.showwarning('', 'Please Complete The Required Field', icon="warning")
+    else:
+        tree.delete(*tree.get_children())
+        conn = sqlite3.connect("pythontut.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO `member` (firstname, lastname, gender, age, address, contact) VALUES(?, ?, ?, ?, ?, ?)", (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), int(AGE.get()), str(ADDRESS.get()), str(CONTACT.get())))
+        conn.commit()
+        cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+        FIRSTNAME.set("")
+        LASTNAME.set("")
+        GENDER.set("")
+        AGE.set("")
+        ADDRESS.set("")
+        CONTACT.set("")
+
+#=====ForUpdateData=====
+def UpdateData():
+    if GENDER.get() == "":
+       result = tkMessageBox.showwarning('', 'Please Complete The Required Field', icon="warning")
+    else:
+        tree.delete(*tree.get_children())
+        conn = sqlite3.connect("pythontut.db")
+        cursor = conn.cursor()
+        cursor.execute("UPDATE `member` SET `firstname` = ?, `lastname` = ?, `gender` =?, `age` = ?,  `address` = ?, `contact` = ? WHERE `mem_id` = ?", (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), str(AGE.get()), str(ADDRESS.get()), str(CONTACT.get()), int(mem_id)))
+        conn.commit()
+        cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+        FIRSTNAME.set("")
+        LASTNAME.set("")
+        GENDER.set("")
+        AGE.set("")
+        ADDRESS.set("")
+        CONTACT.set("")
+        
+#=====ForChangeSomeInformation=====  
+def OnSelected(event):
+    global mem_id, UpdateWindow
+    curItem = tree.focus()
+    contents =(tree.item(curItem))
+    selecteditem = contents['values']
+    mem_id = selecteditem[0]
+    FIRSTNAME.set("")
+    LASTNAME.set("")
+    GENDER.set("")
+    AGE.set("")
+    ADDRESS.set("")
+    CONTACT.set("")
+    FIRSTNAME.set(selecteditem[1])
+    LASTNAME.set(selecteditem[2])
+    AGE.set(selecteditem[4])
+    ADDRESS.set(selecteditem[5])
+    CONTACT.set(selecteditem[6])
+    UpdateWindow = Toplevel()
+    UpdateWindow.title("Contact List")
+    width = 400
+    height = 300
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = ((screen_width/2) + 450) - (width/2)
+    y = ((screen_height/2) + 20) - (height/2)
+    UpdateWindow.resizable(0, 0)
+    UpdateWindow.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    if 'NewWindow' in globals():
+        NewWindow.destroy()
+
+    #=====FrameForChangeFormationWindow=====
+    FormTitle = Frame(UpdateWindow)
+    FormTitle.pack(side=TOP)
+    ContactForm = Frame(UpdateWindow)
+    ContactForm.pack(side=TOP, pady=10)
+    RadioGroup = Frame(ContactForm)
+    Male = Radiobutton(RadioGroup, text="Male", variable=GENDER, value="Male",  font=('arial', 14)).pack(side=LEFT)
+    Female = Radiobutton(RadioGroup, text="Female", variable=GENDER, value="Female",  font=('arial', 14)).pack(side=LEFT)
+    
+    #=====LabelsChangeWindow=====
+    lbl_title = Label(FormTitle, text="Updating Contacts", font=('arial', 16), bg="#ffdaa7",  width = 300)
+    lbl_title.pack(fill=X)
+    lbl_firstname = Label(ContactForm, text="Firstname", font=('arial', 14), bd=5)
+    lbl_firstname.grid(row=0, sticky=W)
+    lbl_lastname = Label(ContactForm, text="Lastname", font=('arial', 14), bd=5)
+    lbl_lastname.grid(row=1, sticky=W)
+    lbl_gender = Label(ContactForm, text="Gender", font=('arial', 14), bd=5)
+    lbl_gender.grid(row=2, sticky=W)
+    lbl_age = Label(ContactForm, text="Age", font=('arial', 14), bd=5)
+    lbl_age.grid(row=3, sticky=W)
+    lbl_address = Label(ContactForm, text="Address", font=('arial', 14), bd=5)
+    lbl_address.grid(row=4, sticky=W)
+    lbl_contact = Label(ContactForm, text="Contact", font=('arial', 14), bd=5)
+    lbl_contact.grid(row=5, sticky=W)
+
+    #=====EntryChangeWindow=====
+    firstname = Entry(ContactForm, textvariable=FIRSTNAME, font=('arial', 14))
+    firstname.grid(row=0, column=1)
+    lastname = Entry(ContactForm, textvariable=LASTNAME, font=('arial', 14))
+    lastname.grid(row=1, column=1)
+    RadioGroup.grid(row=2, column=1)
+    age = Entry(ContactForm, textvariable=AGE,  font=('arial', 14))
+    age.grid(row=3, column=1)
+    address = Entry(ContactForm, textvariable=ADDRESS,  font=('arial', 14))
+    address.grid(row=4, column=1)
+    contact = Entry(ContactForm, textvariable=CONTACT,  font=('arial', 14))
+    contact.grid(row=5, column=1)
+    
+
+    #=====BottonConfirm=====
+    btn_updatecon = Button(ContactForm, text="Update", width=50, command=UpdateData)
+    btn_updatecon.grid(row=6, columnspan=2, pady=10)
+
+
+#=====ForDeleteData=====   
+def DeleteData():
+    if not tree.selection():
+       result = tkMessageBox.showwarning('', 'Please Select Something First!', icon="warning")
+    else:
+        result = tkMessageBox.askquestion('', 'Are you sure you want to delete this record?', icon="warning")
+        if result == 'yes':
+            curItem = tree.focus()
+            contents =(tree.item(curItem))
+            selecteditem = contents['values']
+            tree.delete(curItem)
+            conn = sqlite3.connect("pythontut.db")
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM `member` WHERE `mem_id` = %d" % selecteditem[0])
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+#=====ForAddNewContact=====
+def AddNewWindow():
+    global NewWindow
+    FIRSTNAME.set("")
+    LASTNAME.set("")
+    GENDER.set("")
+    AGE.set("")
+    ADDRESS.set("")
+    CONTACT.set("")
+    NewWindow = Toplevel()
+    NewWindow.title("Add Newcontact List")
+    width = 400
+    height = 300
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = ((screen_width/2) - 455) - (width/2)
+    y = ((screen_height/2) + 20) - (height/2)
+    NewWindow.resizable(0, 0)
+    NewWindow.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    if 'UpdateWindow' in globals():
+        UpdateWindow.destroy()
+    
+    #=====FrameForNewCon=====
+    FormTitle = Frame(NewWindow)
+    FormTitle.pack(side=TOP)
+    ContactForm = Frame(NewWindow)
+    ContactForm.pack(side=TOP, pady=10)
+    RadioGroup = Frame(ContactForm)
+    Male = Radiobutton(RadioGroup, text="Male", variable=GENDER, value="Male",  font=('arial', 14)).pack(side=LEFT)
+    Female = Radiobutton(RadioGroup, text="Female", variable=GENDER, value="Female",  font=('arial', 14)).pack(side=LEFT)
+    
+    #=====LabelsForNewCon=====
+    lbl_title = Label(FormTitle, text="Adding New Contacts", font=('arial', 16), bg="#77d998",  width = 300)
+    lbl_title.pack(fill=X)
+    lbl_firstname = Label(ContactForm, text="Firstname", font=('arial', 14), bd=5)
+    lbl_firstname.grid(row=0, sticky=W)
+    lbl_lastname = Label(ContactForm, text="Lastname", font=('arial', 14), bd=5)
+    lbl_lastname.grid(row=1, sticky=W)
+    lbl_gender = Label(ContactForm, text="Gender", font=('arial', 14), bd=5)
+    lbl_gender.grid(row=2, sticky=W)
+    lbl_age = Label(ContactForm, text="Age", font=('arial', 14), bd=5)
+    lbl_age.grid(row=3, sticky=W)
+    lbl_address = Label(ContactForm, text="Address", font=('arial', 14), bd=5)
+    lbl_address.grid(row=4, sticky=W)
+    lbl_contact = Label(ContactForm, text="Contact", font=('arial', 14), bd=5)
+    lbl_contact.grid(row=5, sticky=W)
+
+    #=====EntryForTypeInformation=====
+    firstname = Entry(ContactForm, textvariable=FIRSTNAME, font=('arial', 14))
+    firstname.grid(row=0, column=1)
+    lastname = Entry(ContactForm, textvariable=LASTNAME, font=('arial', 14))
+    lastname.grid(row=1, column=1)
+    RadioGroup.grid(row=2, column=1)
+    age = Entry(ContactForm, textvariable=AGE,  font=('arial', 14))
+    age.grid(row=3, column=1)
+    address = Entry(ContactForm, textvariable=ADDRESS,  font=('arial', 14))
+    address.grid(row=4, column=1)
+    contact = Entry(ContactForm, textvariable=CONTACT,  font=('arial', 14))
+    contact.grid(row=5, column=1)
+    
+
+    #=====BottonForSaveChange=====
+    btn_addcon = Button(ContactForm, text="Save", width=50, command=SubmitData)
+    btn_addcon.grid(row=6, columnspan=2, pady=10)
+
+
+
+
+    
 #=====FrameOnTop=====
 Top = Frame(root, width=500, bd=1, relief=SOLID)
 Top.pack(side=TOP)
@@ -53,8 +261,24 @@ TableMargin = Frame(root, width=500)
 TableMargin.pack(side=TOP)
 
 #=====Label On Top=====
-lbl_title = Label(Top, text="PSCP Contact Management System",bg="#ffc77b", font=('arial', 16), width=500)
+lbl_title = Label(Top, text="PSCP Contact Management System",bg="#ffc77b", fg="#3B3301", font=('arial', 16), width=500)
 lbl_title.pack(fill=X)
+
+
+
+
+
+
+
+
+
+
+
+#=====Main page Botton=====
+btn_add = Button(MidLeft, text="+ ADD NEW", bg="#229a80", fg="#eefffc", font=('arial', 16), command=AddNewWindow)
+btn_add.pack()
+btn_delete = Button(MidRight, text="DELETE", bg="#f34233", fg="#eefffc", font=('arial', 16), command=DeleteData)
+btn_delete.pack(side=RIGHT)
 
 #=====Show Formation=====
 scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
@@ -80,8 +304,9 @@ tree.column('#5', stretch=NO, minwidth=0, width=80)
 tree.column('#6', stretch=NO, minwidth=0, width=120)
 tree.column('#7', stretch=NO, minwidth=0, width=120)
 tree.pack()
+tree.bind('<Double-Button-1>', OnSelected)
 
-
+#=====For Run The Program=====
 if __name__ == '__main__':
     Database()
     root.mainloop()
